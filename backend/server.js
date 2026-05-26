9c63b098dbf39b09025d1d2e6129b551ebd00841
Ritvik Shinde <ritviksshinde16@gmail.com>
2026-05-26 22:03:50 +0530

    fix:express v5 compatible CORS preflight wildcard

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+app.options('/{*path}', cors());
 
 // Middleware
 app.use(express.json());
